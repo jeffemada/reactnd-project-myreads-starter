b69f102de8769178d23708cb4960507ebd1df759
Jefferson Madalena <jefferson.madalena@gmail.com>
2019-02-02 12:15:45 -0200

    Created a constant to represent shelves.

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const SHELVES = [
+  { id: "wantToRead", title: "Want to Read" },
+  { id: "currentlyReading", title: "Currently Reading" },
+  { id: "read", title: "Read" },
+  { id: "none", title: "None" }
+];
