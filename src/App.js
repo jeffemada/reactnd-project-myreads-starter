fd1bac98911c78bc619fcb23c587dedc181bc3e9
Jefferson Madalena <jefferson.madalena@gmail.com>
2019-02-02 19:08:02 -0200

    Removed unnecessary code.

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@ import SearchBooks from './SearchBooks';
 
 class BooksApp extends Component {
   state = {
-    books: [],
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
   };
 
   /**
@@ -25,7 +18,7 @@ class BooksApp extends Component {
   }
 
   /**
-   * Retorna todos os meus livros.
+   * Retorna todos os livros que estão na estante.
    */
   getAllMyBooks = () => {
     BooksAPI.getAll().then((books) => {
@@ -33,27 +26,13 @@ class BooksApp extends Component {
     });
   };
 
-  search = (query) => {
-    BooksAPI.search(query).then((books) => {
-      console.log('Search books', books);
-    });
-  };
-
-  openSearch = () => {
-    this.setState({ showSearchPage: true });
-  };
-
-  closeSearch = () => {
-    this.setState({ showSearchPage: false });
-  };
-
   /*
+  TODO: implementar se componente precisa ser renderizado
   TODO: tratamento de erros https://reactjs.org/docs/error-boundaries.html
   */
 
   render() {
     const { books } = this.state;
-    console.log('App render');
 
     return (
       <div className="app">
